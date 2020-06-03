4d0b7b14f4bb57960d16270693710b49aea531d3
taminhhienmor <56866116+taminhhienmor@users.noreply.github.com>
2020-06-03 16:54:38 +0900

    add feature change staus

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 ## Introduction
-
+<p>With this node, you can change status in <a href="https://app.slack.com/">Slack</a>.</p>
 
 ## Features
-
+Change status
 
 ## Certificate
 
 
 ## Usage
+
+![slag-node](https://cdn.jsdelivr.net/gh/taminhhienmor/node-red-contrib-slack-status/source/image/node_slack.png)
+
+![slag-node-result](https://cdn.jsdelivr.net/gh/taminhhienmor/node-red-contrib-slack-status/source/image/node_slack_status.png)
+
+``` node
+[{"id":"98ac7b32.233a28","type":"slackStatus","z":"c7c652c3.cc73c","slackCertificate":"355e1241.0432de","statusContent":"hello","statusType":"str","icon":"😀","x":530,"y":800,"wires":[["ece1b118.30a71"]]},{"id":"f8433e01.0e5a2","type":"inject","z":"c7c652c3.cc73c","name":"","topic":"","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":300,"y":800,"wires":[["98ac7b32.233a28"]]},{"id":"ece1b118.30a71","type":"debug","z":"c7c652c3.cc73c","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","x":770,"y":800,"wires":[]},{"id":"355e1241.0432de","type":"slackCertificate","z":""}]
+```
--- source/slackStatus.js
+++ source/slackStatus.js
@@ -6,70 +6,87 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, n);
         var node = this;
 
-        var methodValue = n.method
-        var childPathProperty = n.childpath || ""
-        var propertyType = n.propertyType || "msg";
+        var icon = n.icon
+        var statusProperty = n.statusContent || ""
+        var statusType = n.statusType || "msg";
         var globalContext = this.context().global;
         var flowContext = this.context().flow;
 
         var slackCertificate = RED.nodes.getNode(n.slackCertificate);
 
+        const LINK_URL = "https://slack.com/api/users.profile.set"
+        const STATUS_EXP = 0;
+        const METHOD_POST = "POST"
+
         node.on("input", function (msg) {
             node.status({});
 
-            // select method 
-            switch (methodValue) {
-                case "set":
-                    methodValue = "PUT"
-                    break;
-                case "push":
-                    methodValue = "POST"
-                    break;
-                case "update":
-                    methodValue = "PATCH"
-                    break;
-                case "remove":
-                    methodValue = "DELETE"
-                    break;
-                default:
-                    methodValue = methodValue
-                    break;
+            // check token input
+            if (typeof(slackCertificate.token) == "undefined") {
+                node.error("Token is not empty", {});
+                node.status({fill: "red", shape: "ring", text: "Token is not empty"});
+                return;
             }
 
-            // select childPath
-            var childPath = "";
-            switch (propertyType) {
+            // get status
+            var status = "";
+            switch (statusType) {
                 case "str":
-                    childPath = childPathProperty
+                    status = statusProperty
                     break;
                 case "msg":
-                    childPath = msg[childPathProperty]
+                    status = msg[statusProperty]
                     break;
                 case "flow":
-                    childPath = flowContext.get(childPathProperty)
+                    status = flowContext.get(statusProperty)
                     break;
                 case "global":
-                    childPath = globalContext.get(childPathProperty)
+                    status = globalContext.get(statusProperty)
                     break;
                 default:
-                    childPath = childPathProperty
+                    status = statusProperty
                     break;
             }
 
-            if (methodValue == "setPriority" || methodValue == "setWithPriority") {
-                methodValue = "put"
-            } else if (methodValue == "msg.method" || methodValue == "") {
-                methodValue = msg.method
+            // check msg icon
+            if(icon == "msg.icon") icon = msg.icon
+
+            // change status profile
+            var profileObj = {
+                "profile": {
+                    "status_text": status,
+                    "status_emoji": icon,
+                    "status_expiration": STATUS_EXP
+                }
+            }          
+            
+            // add object request param
+            var opts = {
+                method: METHOD_POST,
+                url: LINK_URL,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + slackCertificate.token
+                },
+                body: JSON.stringify(profileObj)
             };
 
-            if (typeof msg.payload != 'number') {
-                newObj = msg.payload
-            }
-
-            if (typeof newObj != 'object') {
-                newObj = JSON.parse(newObj)
-            };
-
+            request(opts, function (error, response, body) {
+                if (error) {
+                    node.error(error, {});
+                    node.status({fill: "red", shape: "ring", text: "failed"});
+                    return;
+                } else {
+                    if(!JSON.parse(body).ok) {
+                        node.error(JSON.parse(body).error, {});
+                        node.status({fill: "red", shape: "ring", text: "failed"});
+                        return;
+                    } else {
+                        msg.payload =  "Change status successfully";
+                        node.send(msg);
+                    }
+                }    
+            })
         })
     }
 
